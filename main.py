7e469a6878eb7f42b0136d2a94201aa54b4f2687
bowbowbow <clsrn1581@gmail.com>
2019-06-07 14:53:22 +0900

    update main

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import os
-import csv
-from config import Config
+from parser import Parser
+import json
 
 
-def get_data_paths():
-    data_paths = {
-        'dev': [],
-        'test': [],
-        'train': [],
-    }
+def get_data_paths(ace2005_path):
+    test_files, dev_files, train_files = [], [], []
+
     with open('./file_list.csv', mode='r') as csv_file:
         rows = csv_file.readlines()
         for row in rows[1:]:
             items = row.replace('\n', '').split(',')
             data_type = items[0]
             name = items[1]
-            data_paths[data_type].append(os.path.join(Config.ace2005_path, name + '.apf.xml'))
-    return data_paths
+
+            path = os.path.join(ace2005_path, name + '.apf.xml')
+            if data_type == 'test':
+                test_files.append(path)
+            elif data_type == 'dev':
+                dev_files.append(path)
+            elif data_type == 'train':
+                train_files.append(path)
+    return test_files, dev_files, train_files
+
+
+def preprocessing(data_type, files):
+    data = []
+    for file in files:
+        data.extend(Parser(xml_path=file).get_data())
+    with open('output/{}.json'.format(data_type), 'w') as f:
+        json.dump(data[0], f, indent=4)
 
 
 if __name__ == '__main__':
-    data_paths = get_data_paths()
-    print(data_paths)
-    pass
+    test_files, dev_files, train_files = get_data_paths('./data/ace_2005_td_v7/data/English')
+    preprocessing('test', test_files)
+    preprocessing('train', train_files)
+    preprocessing('dev', dev_files)
